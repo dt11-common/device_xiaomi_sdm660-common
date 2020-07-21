5d733652acc7559d41d1cc3b98599bdbcfc5c560
marshmello61 <ultramayur123@gmail.com>
2020-07-21 19:41:03 +0700

    ysl: Add Prebuilt Via

--- sdm660.mk
+++ sdm660.mk
@@ -438,5 +438,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# Via
+PRODUCT_PACKAGES += \
+    Via
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
