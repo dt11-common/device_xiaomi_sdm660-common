258504dd88a6b83326c2bbe63535ffd8572a1c8b
Ubuntu <iqbal@iqbal.heymnpl2vh0ujozbrymtoeynvb.cwx.internal.cloudapp.net>
2021-03-21 13:37:21 +0000

    Revert "whyred: Lineage-fy"
    This reverts commit 17335eee22b184a9473ea7c37ba25625704ef01f.

--- sdm660.mk
+++ sdm660.mk
@@ -28,7 +28,7 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-pe \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
